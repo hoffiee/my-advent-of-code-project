37c0e25cf3cb8b9379a7ff77f2befe0a2ef214d2
hoffiee <richen_mjau@hotmail.com>
2023-12-03 12:39:12 +0100

    2023: day 2, 2 stars

---
+++ 2023/day02/day02.cpp
@@ -0,0 +1,85 @@
+/**
+ * https://adventofcode.com/2023/day/2
+ *
+ * This is where it would be nice to parse data and solve both parts in one go
+ * but I'm ok with the current solution
+ */
+#include AOC_HEADER
+
+#include <algorithm>
+#include <icecream.hpp>
+#include <numeric>
+#include <sstream>
+
+// TODO: Make a string util library
+static std::vector<std::string> split_string(std::string str, char delim) {
+    std::stringstream ss(str);
+    std::string tmp;
+    std::vector<std::string> out;
+    while (getline(ss, tmp, delim)) {
+        out.push_back(tmp);
+    }
+    return out;
+}
+
+static std::vector<int> get_counts(std::string hand) {
+    int cubes = 0;
+    std::string colour;
+
+    std::vector<int> count(3, 0);
+    const std::vector<char> colour_index{'r', 'g', 'b'};
+
+    std::stringstream sshand(hand);
+    while (sshand >> cubes >> colour) {
+        for (size_t i = 0; i < colour_index.size(); i++) {
+            if (colour.at(0) == colour_index.at(i)) {
+                count.at(i) += cubes;
+                break;
+            }
+        }
+    }
+    return count;
+}
+
+int solve_1(std::vector<std::string> inp) {
+    int count_valid_ids = 0;
+    for (auto game : inp) {
+        std::stringstream ss(game);
+
+        std::string ignore;
+        int id = 0;
+        ss >> ignore >> id;
+
+        auto hands = split_string(game.substr(game.find(":") + 2), ';');
+        bool game_valid = true;
+        for (auto hand : hands) {
+            auto count = get_counts(hand);
+            if (count.at(0) > 12 || count.at(1) > 13 || count.at(2) > 14) {
+                game_valid = false;
+            }
+        }
+        if (game_valid) {
+            count_valid_ids += id;
+        }
+    }
+    return count_valid_ids;
+}
+
+int solve_2(std::vector<std::string> inp) {
+    int sum_of_sets_power = 0;
+    for (auto game : inp) {
+        std::stringstream ss(game);
+
+        std::vector<int> min_cubes(3, 0);
+        auto hands = split_string(game.substr(game.find(":") + 2), ';');
+        for (auto hand : hands) {
+            auto count = get_counts(hand);
+            for (size_t i = 0; i < min_cubes.size(); i++) {
+                min_cubes.at(i) = std::max({min_cubes.at(i), count.at(i)});
+            }
+        }
+        sum_of_sets_power += std::accumulate(min_cubes.begin(), min_cubes.end(), 1, std::multiplies<int>());
+    }
+
+    return sum_of_sets_power;
+}
---
+++ 2023/day02/test_day02.cpp
@@ -0,0 +1,32 @@
+#include "gtest/gtest.h"
+
+#include AOC_HEADER
+
+TEST(AOC_TEST, test_1) {
+    std::vector<std::string> inp = {
+        "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+        "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+        "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+        "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+        "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+    };
+    auto out = solve_1(inp);
+    ASSERT_EQ(out, 8);
+}
+
+TEST(AOC_TEST, test_2) {
+    std::vector<std::string> inp = {
+        "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+        "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+        "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+        "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+        "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+    };
+    auto out = solve_2(inp);
+    ASSERT_EQ(out, 2286);
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
