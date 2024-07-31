d5c3e451d024bf4c53589e92edc5c37126aea38d
Rickard <rickard.a1992@gmail.com>
2024-07-31 13:25:03 +0200

    cmake: clean output from jupyter notebooks

--- cmake/jupyter.cmake
+++ cmake/jupyter.cmake
@@ -7,9 +7,11 @@
 # or alternatively do it with jq for SPEEEEED according to:
 #   https://stackoverflow.com/questions/28908319/how-to-clear-jupyter-notebooks-output-in-all-cells-from-the-linux-terminal/58004619#58004619
 #   https://timstaley.co.uk/posts/making-git-and-jupyter-notebooks-play-nice/
+# another alternative:
+#   https://nbdev.fast.ai/tutorials/git_friendly_jupyter.html
 add_custom_target(git-prepare-jupyter
     COMMAND find 20* -type f -name '*.ipynb'
         | grep -v ipynb_checkpoint
-        | xargs -i{} jupyter nbconvert --ClearOutputPreprocessor.enabled=False --ClearMetadataPreprocessor.enabled=True --inplace {}
+        | xargs -i{} jupyter nbconvert --ClearOutputPreprocessor.enabled=True --ClearMetadataPreprocessor.enabled=True --inplace {}
     WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
 )
