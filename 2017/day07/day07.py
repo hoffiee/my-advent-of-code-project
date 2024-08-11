cfeb17731088addddc9df1ae922601a593607d4a
Rickard <rickard.a1992@gmail.com>
2024-08-11 22:53:58 +0200

    2017: day 7 complete

---
+++ 2017/day07/day07.py
@@ -0,0 +1,94 @@
+"""
+Found out python introduced structural pattern matching, so I used that for solution 1
+
+Had some issues with my own solution for part 2, and then read some hints and found that a DFS
+and assuming the first encounter of a branch weight is correct, we can continue until we find one that
+doesn't meet our assumption. I then added so that my algorithm shuffles the children at each step so
+that it founds it.
+"""
+import random
+import statistics
+from collections import defaultdict
+
+
+def sol1(data):
+    """Create a tree where each entry points to it's parent. Then look for an entry that doesn't have a parent"""
+    tree = defaultdict(str)
+    for datum in data:
+        match datum.replace(",", "").split(" "):
+            case [node, _, "->", *childrens]:
+                if node not in tree:
+                    tree[node] = ""
+                for child in childrens:
+                    tree[child] = node
+            case [node, _]:
+                pass
+    for k,v in tree.items():
+        if v == "":
+            return k
+    return ""
+
+
+class DFS():
+    def __init__(self):
+        self.found = False
+        self.corrected_weight = 0
+
+    def reset(self):
+        self.found = False
+        self.corrected_weight = 0
+
+    def dfs(self, children, weights, root):
+        expected = None
+        program_weight = weights[root]
+        random.shuffle(children[root])
+        for child in children[root]:
+            weight = self.dfs(children, weights, child)
+            program_weight += weight
+            # We pick the first child as an expected weight, if any of the following children have a different weight
+            # then we've identified a faulty child, it could be the one we picked as expected or it could be the
+            # following ones we checked. This is why we rerun this with shuffled children in order to find the faulty
+            # one, there are only 1 faulty and we often have more than one children, therefore we should more often
+            # pick a non-faulty one as expected.
+            if expected is None:
+                expected = weight
+            elif not self.found and expected != weight:
+                self.corrected_weight = weights[child] - (weight - expected)
+                self.found = True
+        return program_weight
+
+
+def sol2(data, root):
+    weights = {}
+    children = defaultdict(list)
+
+    for datum in data:
+        datum = datum.strip().split()
+
+        node = datum[0]
+        weights[node] = int(datum[1].strip("()"))
+
+        if "->" in datum:
+            children[node] = [child.strip(",") for child in datum[3:]]
+
+    # Due to randomly choosing correct value, we run this a few times so that we can
+    # see which value occurs more than once
+    solver = DFS()
+    corrected_weights = []
+    for _ in range(10):
+        solver.reset()
+        solver.dfs(children, weights, root)
+        corrected_weights.append(solver.corrected_weight)
+    print(f"correct weight: {int(statistics.median(corrected_weights))}")
+
+
+def main() -> None:
+    for file in ["day07-sample.input", "day07.input"]:
+        with open(file, "r", encoding="utf8") as file:
+            lines = file.read().splitlines()
+            print(root := sol1(lines))
+            sol2(lines, root)
+
+
+if __name__ == "__main__":
+    main()
