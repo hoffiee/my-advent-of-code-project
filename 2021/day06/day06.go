f656971a906469971e8e8286b60d53e1a7ef93c5
Rickard <rickard.a1992@gmail.com>
2024-11-15 20:16:19 +0100

    2021: day 6,7 completed 2 stars

---
+++ 2021/day06/day06.go
@@ -0,0 +1,83 @@
+/*
+*
+Another Go attempt.
+
+Keep track of array where each index is the number of Fishes, so we won't have
+to deal with a growing list of Fishes
+*/
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func parse(input string) []int {
+	size := len(input)
+	out := make([]int, (size+1)/2)
+	for i, digit := range strings.Split(input, ",") {
+		value, err := strconv.Atoi(digit)
+		if err != nil {
+			log.Fatal(err)
+		}
+		out[i] = value
+	}
+	return out
+}
+
+type Fishes [9]int
+
+// Not sure what creating methods like this is called, but trying it out
+func (f *Fishes) step() Fishes {
+	out := Fishes{}
+	for i, val := range f {
+		// internal timer -> 0, reset and add new fish
+		if i == 0 {
+			out[6] = val
+			out[8] = val
+			continue
+		}
+		out[i-1] += val
+	}
+	return out
+}
+
+func (f *Fishes) sum() int {
+	out := 0
+	for _, val := range f {
+		out += val
+	}
+	return out
+}
+
+func solve(initial_fishes []int, steps int) {
+	fishes := Fishes{}
+	for _, val := range initial_fishes {
+		fishes[val] += 1
+	}
+	for _ = range steps {
+		fishes = fishes.step()
+	}
+	fmt.Printf("%d fishes after %d days\n", fishes.sum(), steps)
+}
+
+func main() {
+	// file, err := os.Open("day06-sample.input")
+	file, err := os.Open("day06.input")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		input := parse(line)
+		solve(input, 80)
+		solve(input, 256)
+	}
+}
---
+++ 2021/day07/day07.go
@@ -0,0 +1,101 @@
+/*
+Another Go attempt
+
+part 1: Find the median position
+part 2: Find the average position
+*/
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func parse(input string) []int {
+	entries := strings.Split(input, ",")
+	out := make([]int, len(entries))
+	for i, digit := range entries {
+		value, err := strconv.Atoi(digit)
+		if err != nil {
+			log.Fatal(err)
+		}
+		out[i] = value
+	}
+	return out
+}
+
+func findMedian(positions []int) int {
+	sort.Ints(positions)
+
+	length := len(positions)
+	if length == 0 {
+		return 0
+	}
+
+	if length%2 != 0 {
+		return positions[length/2]
+	}
+	return (positions[(length/2)-1] + positions[length/2]) / 2
+}
+
+func findAverage(positions []int) int {
+	length := len(positions)
+	out := 0
+	for _, val := range positions {
+		out += val
+	}
+	// Get the wrong one without the subtraction of 1e-10
+	return int(float64(out)/float64(length) + 0.5 - 1e-10)
+}
+
+func AbsDiff(x, y int) int {
+	if x < y {
+		return y - x
+	}
+	return x - y
+}
+
+func solve1(positions []int) {
+	position := findMedian(positions)
+	fmt.Println("position:", position)
+	cost := 0
+	for _, val := range positions {
+		addition := AbsDiff(val, position)
+		cost += addition
+	}
+	fmt.Println(cost)
+}
+
+func solve2(positions []int) {
+	position := findAverage(positions)
+	fmt.Println("position:", position)
+
+	cost := 0
+	for _, val := range positions {
+		addition := AbsDiff(val, position)
+		cost += addition * (addition + 1) / 2
+	}
+	fmt.Println(cost)
+}
+
+func main() {
+	// file, err := os.Open("day07-sample.input")
+	file, err := os.Open("day07.input")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		input := parse(line)
+		solve1(input)
+		solve2(input)
+	}
+}
