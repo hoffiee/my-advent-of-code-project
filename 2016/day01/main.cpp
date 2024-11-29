e00a4b64c04b8a54d8c8251118eaa31e82a54798
Rickard <rickard.a1992@gmail.com>
2024-11-29 20:25:53 +0100

    cmake: align pattern for 2016

--- 2016/day01/main.cpp
+++ 2016/day01/main.cpp
@@ -8,9 +8,9 @@
 
 int main() {
     std::ifstream input_file;
-    input_file.open(AOC_TASK ".input");
+    input_file.open(AOC_INPUT);
     if (!input_file.is_open()) {
-        std::cout << "couldn't read file" << std::endl;
+        std::cout << "couldn't read file:" AOC_INPUT << std::endl;
         return -1;
     }
     std::string line;
