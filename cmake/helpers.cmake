c9510cdd048b78a20bb9da3fa42bc18e531610e8
Rickard <rickard.a1992@gmail.com>
2024-11-29 20:24:44 +0100

    cmake: align pattern for 2015

--- cmake/helpers.cmake
+++ cmake/helpers.cmake
@@ -3,3 +3,15 @@ macro(aoc_copy_input_file)
         DESTINATION ${CMAKE_CURRENT_BINARY_DIR}
     )
 endmacro()
+
+
+macro(aoc_set_task_variables)
+    get_filename_component(AOC_DAY ${CMAKE_CURRENT_SOURCE_DIR} NAME)
+    set(TASK ${AOC_YEAR}_${AOC_DAY})
+    add_definitions(
+        -DAOC_TEST=year_${TASK}
+        -DAOC_TASK="${TASK}"
+        -DAOC_INPUT="${AOC_DAY}.input"
+        -DAOC_HEADER="${AOC_DAY}.h"
+    )
+endmacro()
