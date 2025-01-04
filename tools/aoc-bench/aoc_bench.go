b202a1f213760944e9ccd03ae0d911acd3c7bd45
Rickard <rickard.a1992@gmail.com>
2025-01-04 20:53:04 +0100

    aoc_bench: refactored a bit

--- tools/aoc-bench/aoc_bench.go
+++ tools/aoc-bench/aoc_bench.go
@@ -1,10 +1,7 @@
 /*
-* AOC benchmarking, it's a wrapper for running hyperfine on part1 1 and part1 2 individually
+AOC benchmarking, it's a wrapper for running hyperfine on part1 1 and part1 2 individually
 
-TODO: Error handling!, or we just validate after parsing and bail out
 TODO: Fix handling of when entry already exists, prompt user for y/n to replace, or flag and rerun?
-TODO: Refactor into something readable too.
-TODO: extend db schema to include more info
 */
 package main
 
@@ -21,20 +18,9 @@ import (
 	"strings"
 )
 
-func main() {
-	db_path := flag.String("db", "./bench.db", "defines the path to a sqlite database")
-	target := flag.String("target", "", "target to benchmark")
-	// overwrite := flag.String("overwrite", "false", "TODO overwrite current entry")
-	flag.Parse()
-	if *target == "" {
-		panic("No target provided")
-	}
-
-	fmt.Println("Benchmarking: ", *target)
-	fmt.Println("\tNote that you have to make sure that the binary is correct and works as expected!")
-
-	part1 := "\"" + *target + " --part=1\""
-	part2 := "\"" + *target + " --part=2\""
+func runBenchmark(target string) string {
+	part1 := "\"" + target + " --part=1\""
+	part2 := "\"" + target + " --part=2\""
 
 	// I only got this working when using bash -c and then sending in the
 	// entire thing as a one liner, there's probably better ways to do this.
@@ -51,8 +37,21 @@ func main() {
 		}
 	}
 	fmt.Println(string(benchOut))
+	return string(benchOut)
+}
 
-	lines := strings.Split(string(benchOut), "\n")
+type benchmarkData struct {
+	year       int
+	day        int
+	part_valid [2]bool
+	mean       [2]float64
+	variance   [2]float64
+	times_min  [2]float64
+	times_max  [2]float64
+}
+
+func newBenchmarkData(benchOut string) *benchmarkData {
+	data := benchmarkData{}
 
 	benchmark_pattern := `Benchmark (\d+): (.*) --part=(\d+)`
 	benchmark_re := regexp.MustCompile(benchmark_pattern)
@@ -68,67 +67,105 @@ func main() {
 	range_pattern := `Range \(min.*`
 	range_re := regexp.MustCompile(range_pattern)
 
-	var year, day, part int = 0, 0, 0
-	var mean [2]float64
-	var variance [2]float64
-	var times_min [2]float64
-	var times_max [2]float64
-
-	for _, line := range lines {
+	part := 0
+	for _, line := range strings.Split(benchOut, "\n") {
 		// alternatively i can use FindAllStringSubmatch
 		match := benchmark_re.FindStringSubmatch(line)
 		if len(match) == 4 {
+			fmt.Println("Parsing", match)
 			part, _ = strconv.Atoi(match[1])
 			binary := filepath.Base(match[2])
 			filename_pattern := `(\d+)_day(\d+)`
 			filename_re := regexp.MustCompile(filename_pattern)
 			match = filename_re.FindStringSubmatch(binary)
-			year, _ = strconv.Atoi(match[1])
-			day, _ = strconv.Atoi(match[2])
+			data.year, _ = strconv.Atoi(match[1])
+			data.day, _ = strconv.Atoi(match[2])
+			data.part_valid[part-1] = true
 			continue
 		}
 
 		match = time_re.FindStringSubmatch(line)
 		if len(match) > 0 {
+			fmt.Println("Parsing", match)
 			match = float_re.FindAllString(match[0], -1)
-			mean[part-1], _ = strconv.ParseFloat(match[0], 64)
-			variance[part-1], _ = strconv.ParseFloat(match[1], 64)
+			data.mean[part-1], _ = strconv.ParseFloat(match[0], 64)
+			data.variance[part-1], _ = strconv.ParseFloat(match[1], 64)
 			continue
 		}
 
 		match = range_re.FindStringSubmatch(line)
 		if len(match) > 0 {
+			fmt.Println("Parsing", match)
 			match = float_re.FindAllString(match[0], -1)
-			times_min[part-1], _ = strconv.ParseFloat(match[0], 64)
-			times_max[part-1], _ = strconv.ParseFloat(match[1], 64)
+			data.times_min[part-1], _ = strconv.ParseFloat(match[0], 64)
+			data.times_max[part-1], _ = strconv.ParseFloat(match[1], 64)
 			continue
 		}
 	}
+	return &data
+}
 
-	// Rethink this check, maybe create struct and have a validate method.
-	if mean[0] == 0 || mean[1] == 0 || variance[0] == 0 || variance[1] == 0 || times_min[0] == 0 || times_min[1] == 0 || times_max[0] == 0 || times_max[1] == 0 {
-		panic("Parsing isn't working as expected")
+func validate_benchmark(benchmark *benchmarkData) bool {
+	for i := range 2 {
+		if !benchmark.part_valid[i] {
+			continue
+		}
+		cond := &benchmark.part_valid[i]
+		*cond = *cond && benchmark.mean[i] > 0
+		*cond = *cond && benchmark.times_min[i] > 0
+		*cond = *cond && benchmark.times_max[i] > 0
 	}
+	return benchmark.part_valid[0] || benchmark.part_valid[1]
+}
 
-	// Send to database
-	db, err := sql.Open("sqlite3", *db_path)
+func store_benchmark(db_path string, benchmark *benchmarkData) {
+	db, err := sql.Open("sqlite3", db_path)
 	if err != nil {
 		log.Fatal("Failed to connect to database:", err)
 	}
 	defer db.Close()
 
-	fmt.Println("Connected to database: ", *db_path)
-	stmt, err := db.Prepare("INSERT INTO entries(year, day, part, milliseconds) VALUES(?, ?, ?, ?)")
+	fmt.Println("Connected to database: ", db_path)
+	stmt, err := db.Prepare("INSERT INTO entries(year, day, part, mean_ms, variance_ms, min_ms, max_ms) VALUES(?, ?, ?, ?, ?, ?, ?)")
 	if err != nil {
 		log.Fatalf("Failed to prepare statement: %v", err)
 	}
 	defer stmt.Close()
 
 	for part_i := range 2 {
-		fmt.Printf("writing: year:%d day:%d part:%d mean:%f\n", year, day, part_i+1, mean[part_i])
-		_, err = stmt.Exec(year, day, part_i+1, mean[part_i])
+		if !benchmark.part_valid[part_i] {
+			continue
+		}
+
+		fmt.Printf("writing: year:%d day:%d part:%d mean:%f\n", benchmark.year, benchmark.day, part_i+1, benchmark.mean[part_i])
+		_, err = stmt.Exec(benchmark.year, benchmark.day, part_i+1, benchmark.mean[part_i],
+			benchmark.variance[part_i],
+			benchmark.times_min[part_i],
+			benchmark.times_max[part_i])
 		if err != nil {
-			log.Fatalf("Failed to execute insert: %v", err)
+			log.Fatalf("Failed to execute insert: %v\n", err)
 		}
 	}
+}
+
+func main() {
+	db_path := flag.String("db", "./bench.db", "defines the path to a sqlite database")
+	target := flag.String("target", "", "target to benchmark")
+	// overwrite := flag.String("overwrite", "false", "TODO overwrite current entry")
+	flag.Parse()
+	if *target == "" {
+		panic("No target provided")
+	}
+
+	fmt.Println("Benchmarking: ", *target)
+	fmt.Println("\tNote that you have to make sure that the binary is correct and works as expected!")
+
+	benchOut := runBenchmark(*target)
+	data := newBenchmarkData(benchOut)
+	valid := validate_benchmark(data)
+	if !valid {
+		panic("Something went wrong for both part 1 and part 2")
+	}
+
+	store_benchmark(*db_path, data)
 }
--- tools/aoc-bench/dbnotes/create-table.sql
+++ tools/aoc-bench/dbnotes/create-table.sql
@@ -3,6 +3,9 @@ CREATE TABLE entries (
     year INTEGER NOT NULL,
     day INTEGER NOT NULL,
     part INTEGER NOT NULL,
-    microsecond INTEGER NOT NULL,
+    mean_ms INTEGER NOT NULL,
+    variance_ms INTEGER NOT NULL,
+    min_ms INTEGER NOT NULL,
+    max_ms INTEGER NOT NULL,
     UNIQUE(year, day, part)  -- Ensures unique combinations of year, day, and part
 );
