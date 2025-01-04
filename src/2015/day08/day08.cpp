0b748d9bd6e4844c37b142b9b758dd278c6871ef
Rickard <rickard.a1992@gmail.com>
2025-01-04 11:49:53 +0100

    2015: day 8

---
+++ src/2015/day08/day08.cpp
@@ -0,0 +1,100 @@
+/**
+ * https://adventofcode.com/2015/day/8
+ */
+#include <algorithm>
+#include <cassert>
+#include <icecream.hpp>
+#include <numeric>
+#include <regex>
+#include <string>
+#include <vector>
+
+#include "aoc_runner.h"
+#include "string_utils.h"
+
+int64_t solve_1(std::vector<std::string> const& inp) {
+    int64_t sum{0};
+    for (auto const& word : inp) {
+        assert(word.front() == '"');
+        assert(word.back() == '"');
+
+        sum += word.size();
+        std::string decoded{};
+        int64_t len{0};
+        for (size_t i{1}; i < word.size() - 1; i++) {
+            if (word[i] != '\\') {
+                decoded += word[i];
+                len++;
+            } else if (word.substr(i, 2) == "\\\"") {
+                decoded += '"';
+                i++;
+                len++;
+            } else if (word.substr(i, 2) == "\\\\") {
+                decoded += '"';
+                i++;
+                len++;
+            } else if (word.substr(i, 2) == "\\x") {
+                int ch{std::stoi(word.substr(i + 2, 2), nullptr, 16)};
+                decoded += static_cast<char>(ch);
+                i += 3;
+                len++;
+            } else {
+                assert(false);
+            }
+        }
+        assert(len == static_cast<int64_t>(decoded.size()));
+        sum -= decoded.size();
+    }
+    return sum;
+}
+
+int64_t solve_2(std::vector<std::string> const& inp) {
+    int64_t sum{0};
+    for (auto const& word : inp) {
+        sum -= word.size();
+        std::string encoded{"\""};
+        for (auto ch : word) {
+            switch (ch) {
+                case '"':
+                    encoded += "\\\"";
+                    break;
+                case '\\':
+                    encoded += "\\\\";
+                    break;
+                default:
+                    encoded += ch;
+                    break;
+            }
+        }
+        encoded += "\"";
+        sum += encoded.size();
+    }
+    return sum;
+}
+
+void samples() {
+    auto sample = string_utils::read_input(AOC_SAMPLE_INPUT);
+    assert(solve_1(sample) == 12);
+    assert(solve_2(sample) == 19);
+
+    auto sample1 = string_utils::read_input("day08-sample-1.input");
+    assert(solve_1(sample1) == 3 - 1 + 6 - 2 + 8 - 2);
+}
+
+int main(int argc, char** argv) {
+    auto input = string_utils::read_input(AOC_INPUT);
+
+    auto solve_1_wrapper = [](std::vector<std::string> const& inp) -> void {
+        auto part1 = solve_1(inp);
+        assert(part1 == 1333);
+        std::cout << "part 1: " << part1 << std::endl;
+    };
+
+    auto solve_2_wrapper = [](std::vector<std::string> const& inp) -> void {
+        auto part2 = solve_2(inp);
+        assert(part2 == 2046);
+        std::cout << "part 2: " << part2 << std::endl;
+    };
+
+    return aoc::run(argc, argv, samples, solve_1_wrapper, solve_2_wrapper, input);
+}
