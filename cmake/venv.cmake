82d1dc40821c37322e2f1d1598cfc076331a4a76
Rickard <rickard.a1992@gmail.com>
2025-02-15 13:27:36 +0100

    cmake: update venv targets

--- cmake/venv.cmake
+++ cmake/venv.cmake
@@ -7,12 +7,12 @@ set(VENV_PYTHON ${CMAKE_BINARY_DIR}/venv/bin/python)
 set(VENV_PIP ${CMAKE_BINARY_DIR}/venv/bin/pip)
 
 add_custom_target(venv-create
-    COMMAND python3 -m venv venv
-    WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
+    COMMAND ${Python3_EXECUTABLE} -m venv venv
+    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
 )
 
 add_custom_target(venv-update
     DEPENDS venv-create ${PROJECT_SOURCE_DIR}/requirements.txt
     COMMAND ${VENV_PIP} install -r ${PROJECT_SOURCE_DIR}/requirements.txt --upgrade
-    WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
+    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
 )
