7a758dd25a17591f6f0a9dfd484c691a5fce950b
Rickard <rickard.a1992@gmail.com>
2025-07-29 18:06:30 +0200

    2020: day 7, second star

--- src/2020/day07/day07.py
+++ src/2020/day07/day07.py
@@ -1,5 +1,6 @@
 import re
 import sys
+from functools import cache
 
 from libs.python.aoc_runner import aoc_runner
 
@@ -31,56 +32,59 @@ def sol1(inp):
     return len(sr) - 1
 
 
+class Counter:
+    def __init__(self, inp):
+        inp = list(map(lambda x: x.replace(" bags", ""), inp))
+        inp = list(map(lambda x: x.replace(" bag", ""), inp))
+        inp = list(map(lambda x: x.replace(".", ""), inp))
+        inp = list(map(lambda x: x.split("contain"), inp))
+        inp = list(map(lambda x: [x[0][:-1], x[1].split(",")], inp))
+
+        self.relations = {}
+        for [bag, subbags] in inp:
+            self.relations[bag] = []
+            if "no other" in subbags[0]:
+                continue
+            for subbag in subbags:
+                match = re.match(r".*(\d)\s+(.*)", subbag)
+                if match:
+                    digit = int(match.group(1))
+                    rest = match.group(2)
+                else:
+                    assert False
+                self.relations[bag] = [*self.relations[bag], (digit, rest)]
+
+
+    @cache
+    def count_bags(self, root_bag):
+        assert root_bag in self.relations
+
+        if len(self.relations[root_bag]) == 0:
+            return 1
+
+        count: int = 1
+        for nr_of_bags, subbag in self.relations[root_bag]:
+            bags = self.count_bags(subbag)
+            count += nr_of_bags*self.count_bags(subbag)
+
+        return count
+
+
 def sol2(inp):
-    inp = list(map(lambda x: x.replace("bags", "bag"), inp))
-    inp = list(map(lambda x: x.replace(".", ""), inp))
-    inp = list(map(lambda x: x.split("contain"), inp))
-    inp = list(map(lambda x: [x[0][:-1], x[1].split(",")], inp))
-
-    d = {}
-    for l in inp:
-        nl = {}
-        for n in l[1]:
-            if "no other bag" in n:
-                nl = 0
-            else:
-                nr = [int(s) for s in n.split() if s.isdigit()][0]
-                nl.update({re.sub(r"[0-9]+", "", n).lstrip(): nr})
-        d.update({l[0]: nl})
-
-    for k, v in d.items():
-        print(f"{k}: {v}")
-
-    while True:
-        for top_bag, top_content in d.items():
-            dn = {}
-            if not isinstance(top_content, int):
-                for kk, vv in top_content.items():
-                    print(
-                        f"top_bag: {top_bag}, top_content: {top_content}, kk: {kk}, vv: {vv}"
-                    )
-
-                    if not isinstance(d[kk], int):
-                        for ins_bag, ins_v in d[kk].items():
-                            print(
-                                f"bag: {kk}, mult: {vv}, ins_bag: {ins_bag}, ins_v: {ins_v}"
-                            )
-
-            d[top_bag] = dn
-
-        break
-
-    print()
-    for k, v in d.items():
-        print(f"{k}: {v}")
-
-    return 0
+    counter = Counter(inp)
+    # Each depth includes itself, question specified how many bags shiny gold
+    # contains so deduct one.
+    out = counter.count_bags("shiny gold") - 1
+    return out
 
 
 def samples() -> None:
     sam = read_and_parse_lines("day07-sample.input")
     assert sol1(sam) == 4
-    assert sol2(sam) == 126
+    assert sol2(sam) == 32
+
+    sam1 = read_and_parse_lines("day07-sample-1.input")
+    assert sol2(sam1) == 126
 
 
 if __name__ == "__main__":
