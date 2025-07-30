4aad08d057fd947871ed6f6c24231d3a298027df
Rickard <rickard.a1992@gmail.com>
2025-07-30 22:28:50 +0200

    2020: day 11 part 2

--- src/2020/day11/day11.py
+++ src/2020/day11/day11.py
@@ -12,96 +12,201 @@ def read_and_parse_lines(filename):
     return out
 
 
-def change_seat_adj(s, r, c):
-    rmax = len(s) - 1
-    cmax = len(s[0]) - 1
-
-    # Center
-    if (0 < r < rmax) and (0 < c < cmax):
-        adj = [
-            s[r - 1][c - 1],
-            s[r - 1][c],
-            s[r - 1][c + 1],
-            s[r][c - 1],
-            s[r][c + 1],
-            s[r + 1][c - 1],
-            s[r + 1][c],
-            s[r + 1][c + 1],
-        ]
-
-    # edges
-    elif r == 0 and (0 < c < cmax):
-        adj = [s[r][c - 1], s[r][c + 1], s[r + 1][c - 1], s[r + 1][c], s[r + 1][c + 1]]
-    elif r == rmax and (0 < c < cmax):
-        adj = [s[r - 1][c - 1], s[r - 1][c], s[r - 1][c + 1], s[r][c - 1], s[r][c + 1]]
-    elif (0 < r < rmax) and c == 0:
-        adj = [s[r - 1][c], s[r - 1][c + 1], s[r][c + 1], s[r + 1][c], s[r + 1][c + 1]]
-    elif (0 < r < rmax) and c == cmax:
-        adj = [s[r - 1][c - 1], s[r - 1][c], s[r][c - 1], s[r + 1][c - 1], s[r + 1][c]]
-
-    # corner
-    elif r == 0 and c == 0:
-        adj = [s[r][c + 1], s[r + 1][c], s[r + 1][c + 1]]
-
-    elif r == 0 and c == cmax:
-        adj = [s[r][c - 1], s[r + 1][c - 1], s[r + 1][c]]
-    elif r == rmax and c == 0:
-        adj = [s[r - 1][c], s[r - 1][c + 1], s[r][c + 1]]
-    elif r == rmax and c == cmax:
-        adj = [s[r - 1][c - 1], s[r - 1][c], s[r][c - 1]]
-    # faulty
-    else:
-        adj = ["F"]
-
-    if s[r][c] == "L" and adj.count("#") == 0:
-        return "#", True
-    if s[r][c] == "#" and adj.count("#") >= 4:
-        return "L", True
-    return s[r][c], False
-
-
 def print_seats(l):
     for r in l:
         for s in r:
             print(s, end="")
         print()
+    print()
+
+
+class Seats:
+    def __init__(self, lines):
+        self.seating = list(map(list, lines))
+        self.sz_y = len(self.seating)
+        self.sz_x = len(self.seating[0])
+
+    def valid(self, pos):
+        x, y = pos
+        if y < 0 or self.sz_y <= y:
+            return False
+        if x < 0 or self.sz_x <= x:
+            return False
+        return True
+
+    def __getitem__(self, pos):
+        assert self.valid(pos)
+        return self.seating[pos[1]][pos[0]]
+
+    def update(self, seats):
+        self.seating = list(map(list, seats))
+
+
+dirs = [
+    (
+        -1,
+        -1,
+    ),
+    (
+        0,
+        -1,
+    ),
+    (
+        1,
+        -1,
+    ),
+    (
+        -1,
+        0,
+    ),
+    (
+        1,
+        0,
+    ),
+    (
+        -1,
+        1,
+    ),
+    (
+        0,
+        1,
+    ),
+    (
+        1,
+        1,
+    ),
+]
+
+
+def count_seats_1(seats: Seats, pos):
+    x, y = pos
+    count = 0
+    for dir in dirs:
+        dx, dy = dir
+        cand = (x + dx, y + dy)
+        if not seats.valid(cand):
+            continue
+
+        if seats[cand] == "#":
+            count += 1
+
+    return count
+
+
+def count_seats_2(seats: Seats, pos):
+    x, y = pos
+    count = 0
+    for dir in dirs:
+        dx, dy = dir
+        step = 1
+        while seats.valid((x + step * dx, y + step * dy)):
+            cand = seats[(x + step * dx, y + step * dy)]
+            if cand in ["#", "L"]:
+                count += int(cand == "#")
+                break
+            step += 1
+    return count
+
+
+def change_seat(seats: Seats, counter, threshold, pos):
+    if seats[pos] == ".":
+        return ".", False
+
+    (x, y) = pos
+    match seats[pos]:
+        case "L":
+            count = counter(seats, pos)
+            if count == 0:
+                return "#", True
+            return "L", False
+        case "#":
+            count = counter(seats, pos)
+            if count >= threshold:
+                return "L", True
+            return "#", False
+        case ".":
+            pass
+        case _:
+            assert False
+
+    return seats[pos], False
 
 
 def sol1(lines):
+    seats = Seats(lines)
     while True:
-        cont = False
-        nl = list(map(list, lines))
-        for r in range(len(lines)):
-            for c in range(len(lines[0])):
-                nl[r][c], changed = change_seat_adj(lines, r, c)
+        stop = True
+        nl = list(map(list, seats.seating))
+        for y in range(seats.sz_y):
+            for x in range(seats.sz_x):
+                if seats[(x, y)] == ".":
+                    continue
+
+                new, changed = change_seat(seats, count_seats_1, 4, (x, y))
+                nl[y][x] = new
                 if changed:
-                    cont = changed
-        lines = list(map(list, nl))
-        if not cont:
+                    stop = False
+        seats.update(nl)
+        if stop:
             break
 
-    return sum(l.count("#") for l in lines)
+    return sum(l.count("#") for l in seats.seating)
 
 
 def sol2(lines):
+    seats = Seats(lines)
     while True:
-        cont = False
-        nl = list(map(list, lines))
-        for r in range(len(lines)):
-            for c in range(len(lines[0])):
-                nl[r][c], changed = change_seat_adj(lines, r, c)
-                if changed:
-                    cont = changed
-        lines = list(map(list, nl))
-        print_seats(lines)
-        if not cont:
-            break
-        break
+        stop = True
+        nl = list(map(list, seats.seating))
+        for y in range(seats.sz_y):
+            for x in range(seats.sz_x):
+                if seats[(x, y)] == ".":
+                    continue
 
-    return sum(l.count("#") for l in lines)
+                new, changed = change_seat(seats, count_seats_2, 5, (x, y))
+                nl[y][x] = new
+                if changed:
+                    stop = False
+        seats.update(nl)
+        if stop:
+            break
+
+    return sum(l.count("#") for l in seats.seating)
 
 
 def samples() -> None:
+    inp = [
+        ".......#.",
+        "...#.....",
+        ".#.......",
+        ".........",
+        "..#L....#",
+        "....#....",
+        ".........",
+        "#........",
+        "...#.....",
+    ]
+    assert count_seats_2(Seats(inp), (3, 4)) == 8
+
+    inp = [
+        ".............",
+        ".L.L.#.#.#.#.",
+        ".............",
+    ]
+    assert count_seats_2(Seats(inp), (1, 1)) == 0
+    assert count_seats_2(Seats(inp), (3, 1)) == 1
+
+    inp = [
+        ".##.##.",
+        "#.#.#.#",
+        "##...##",
+        "...L...",
+        "##...##",
+        "#.#.#.#",
+        ".##.##.",
+    ]
+    assert count_seats_2(Seats(inp), (3, 3)) == 0
+
     sam = read_and_parse_lines("day11-sample.input")
     assert sol1(sam) == 37
     assert sol2(sam) == 26
