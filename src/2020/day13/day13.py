76dee1cf5283e3960cc45d542ab0174dbcc898c5
Rickard <rickard.a1992@gmail.com>
2025-08-08 23:21:52 +0200

    2020: day 13 star 2

--- src/2020/day13/day13.py
+++ src/2020/day13/day13.py
@@ -30,26 +30,72 @@ def sol1(lines):
 
 
 def sol2(lines):
-    bi = []
-    tf = []
+    # So we're looking for
+    # x     = n1*x1
+    # x + 1 = n2*x2
+    # x + 2 = n3*x3
+    # ...
+    # for all busses (excluding some)
+
+    # Set up as linear system of Diophantine equations
+    # x = ai + ni*xi   v i in [1, ..., k]
+    # where ni, are pairwise coprime integers, in this case the bus id/period
+    # where x is the common timestamp base
+    # ai is the offset to the timestamp for each bus
+    # xi is the amount of times the bus has gone.
+    # And let's use the Direct construction for Chinese remainder theorem.
+
+    # https://en.wikipedia.org/wiki/Diophantine_equation#Chinese_remainder_theorem
+    # https://en.wikipedia.org/wiki/Chinese_remainder_theorem
+    # https://cp-algorithms.com/algebra/linear-diophantine-equation.html
+    ni = []
+    ai = []
     for i in range(len(lines[1])):
         if not lines[1][i] == "x":
-            bi.append(int(lines[1][i]))
-            tf.append(i)
+            ni.append(int(lines[1][i]))
+            ai.append(-i)
+    nvec = np.array(ni)
 
-    print(bi)
-    print(tf)
+    # Product of all moduli
+    N = np.prod(nvec)
 
-    bi_vec = np.array(bi)
-    tf_vec = np.array(tf)
+    out = 0
+    for i in range(len(ni)):
+        # product of all moduli but one
+        Ni = N // ni[i]
 
-    print(tf_vec / bi_vec)
+        # Find modular multiplicative inverse:
+        # Ni*Mi = 1   mod m
+        # https://cp-algorithms.com/algebra/module-inverse.html
+        Mi = 1
+        while Ni * Mi % ni[i] != 1:
+            Mi += 1
+
+        out += ai[i] * Ni * Mi
+
+    out %= N
+    return out
 
 
 def samples() -> None:
     sam = read_and_parse_lines("day13-sample.input")
     assert sol1(sam) == 295
-    assert sol2(sam) == 3417
+
+    sam1 = read_and_parse_lines("day13-sample-1.input")
+    assert sol2(sam1) == 3417
+
+    sam2 = read_and_parse_lines("day13-sample-2.input")
+    assert sol2(sam2) == 754018
+
+    sam3 = read_and_parse_lines("day13-sample-3.input")
+    assert sol2(sam3) == 779210
+
+    sam4 = read_and_parse_lines("day13-sample-4.input")
+    assert sol2(sam4) == 1261476
+
+    sam5 = read_and_parse_lines("day13-sample-5.input")
+    assert sol2(sam5) == 1202161486
+    assert sol2(sam) == 1068781
 
 
 if __name__ == "__main__":
