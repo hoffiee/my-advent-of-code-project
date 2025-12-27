8a08427407a80c777d494225ae468eaf89064cc1
Rickard <rickard.a1992@gmail.com>
2025-12-27 16:56:59 +0100

    jobs: fixed path for test build all

--- jobs/test_build_all.py
+++ jobs/test_build_all.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
+import subprocess
+import sys
+sys.path.insert(0, subprocess.check_output(['git', 'rev-parse', '--show-toplevel']).decode().strip())
+
 from argparse import Namespace
+
+
 from tools.cmake_utils import cmake_build
+
 
 args = Namespace(debug=True, release=False, targets=["all"])
 res = cmake_build(args)
