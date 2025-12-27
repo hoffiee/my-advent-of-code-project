eb8d9adac4c39ddfe077d9d2938b8e6049fe47ba
Rickard <rickard.a1992@gmail.com>
2025-12-27 16:57:45 +0100

    jobs: added validate all test

---
+++ jobs/test_validate_all.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+"""This test is intended to run all solutions, and as all are implemented in
+such a way that they'll assert if they are incorrect, it's enough to kind of
+just run all solutions and see if any exists with a non-zero exit code.
+"""
+
+import subprocess
+import sys
+from argparse import Namespace
+sys.path.insert(0, subprocess.check_output(['git', 'rev-parse', '--show-toplevel']).decode().strip())
+
+from tools.cmake_utils import cmake_build, cmake_fetch_targets
+
+args = Namespace(debug=True, release=False, targets=["all"])
+targets = cmake_fetch_targets(args, None)
+
+targets_to_run = []
+for target in targets:
+    if target.endswith("-run"):
+        targets_to_run.append(target)
+targets_to_run = sorted(targets_to_run)
+
+args.targets = [*targets_to_run]
+cmake_build(args, None)
--- tools/cmake_utils.py
+++ tools/cmake_utils.py
@@ -1,6 +1,8 @@
 import os
 import subprocess
 
+from typing import List
+
 from tools.conan_utils import setup_conan
 from tools.toolchain_utils import remove_dir_if_exists
 
@@ -27,3 +29,20 @@ def cmake_build(args=None, forward_args=None) -> int:
         cmake_setup(args, forward_args)
     result = subprocess.run(["ninja", *args.targets], cwd=build_path, check=True)
     return result.returncode
+
+
+def cmake_fetch_targets(args=None, forward_args=None) -> List[str]:
+    build_path = os.path.join("build/debug")
+    if args.release:
+        build_path = os.path.join("build/release")
+    if not os.path.exists(build_path):
+        cmake_setup(args, forward_args)
+
+    result = subprocess.check_output(
+        ["ninja", "-t", "targets"], cwd=build_path, text=True
+    ).splitlines()
+
+    # Remove internal targets and remove : phony etc.
+    result = [line.split(":")[0] for line in result if "CMakeFiles" not in line]
+
+    return result
