d691225e9bba3c31812937b70c696e4780b44c17
Rickard <rickard.a1992@gmail.com>
2025-12-31 00:43:43 +0100

    cmake/venv: fix so that target is only updated when needed

--- cmake/venv.cmake
+++ cmake/venv.cmake
@@ -8,13 +8,20 @@ set(VENV_PYTEST ${CMAKE_BINARY_DIR}/venv/bin/pytest)
 set(VENV_PYTHON ${CMAKE_BINARY_DIR}/venv/bin/python)
 set(VENV_PIP ${CMAKE_BINARY_DIR}/venv/bin/pip)
 
-add_custom_target(venv-create
-    COMMAND ${Python3_EXECUTABLE} -m venv venv
+# Set a stamp so that the venv-update is only ran when requirements.txt has changed
+set(VENV_STAMP "${CMAKE_BINARY_DIR}/venv_stamp.txt")
+# Remove to force check
+file(REMOVE "${VENV_STAMP}")
+
+add_custom_command(
+    OUTPUT ${VENV_STAMP}
+    COMMAND ${Python3_EXECUTABLE} -m venv venv --clear  # clear forces recreate if needed
+    COMMAND ${VENV_PIP} install -r ${PROJECT_SOURCE_DIR}/requirements.txt --upgrade
+    COMMAND ${CMAKE_COMMAND} -E touch ${VENV_STAMP}
+    DEPENDS ${PROJECT_SOURCE_DIR}/requirements.txt
     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+    COMMENT "Updating venv from ${PROJECT_SOURCE_DIR}/requirements.txt"
+    VERBATIM
 )
 
-add_custom_target(venv-update
-    DEPENDS venv-create ${PROJECT_SOURCE_DIR}/requirements.txt
-    COMMAND ${VENV_PIP} install -r ${PROJECT_SOURCE_DIR}/requirements.txt --upgrade
-    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-)
+add_custom_target(venv-update DEPENDS ${VENV_STAMP})
